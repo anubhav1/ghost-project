8a35eddbbe8e3c04f39733418b7bb6b9fa375fca
anubhav.jhalani@nordcloud.com <anubhav.jhalani@nordcloud.com>
2023-05-14 21:32:01 +0200

    syntax error fixed

--- ec2.tf
+++ ec2.tf
@@ -117,7 +117,7 @@ resource "aws_launch_template" "ghostLaunchTemplate" {
   monitoring {
     enabled = true
   }
-  vpc_security_group_ids = ["${ws_security_group.ec2-sg.id}"]
+  vpc_security_group_ids = ["${aws_security_group.ec2-sg.id}"]
   tag_specifications {
     resource_type = "instance"
 
