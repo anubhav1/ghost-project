bc228dfce3330757914d8ae469ee2c9ddf6b6f26
anubhav.jhalani@nordcloud.com <anubhav.jhalani@nordcloud.com>
2023-05-14 23:24:27 +0200

    test

--- provider.tf
+++ provider.tf
@@ -15,44 +15,14 @@ provider "aws" {
   region  = "${var.primary_region}"
 }
 
-
-#Resources to save terraform states
-resource "aws_dynamodb_table" "terraform-state-lock-dynamo" {
-    name = "${var.tf_state_lock_db}"
-    hash_key         = "LockID"
-    billing_mode   = "PROVISIONED"
-    read_capacity  = 20
-    write_capacity = 20
-    attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-resource "aws_s3_bucket" "terraform-state-save-bucket" {
-  bucket = "${var.tf_state_save_bucket}"
-  tags = {
-    Name        = "My bucket"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform-state-save-bucket-block" {
-  bucket = aws_s3_bucket.terraform-state-save-bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 # Specifies the S3 Bucket and DynamoDB table used for the durable backend and state locking
 terraform {
     backend "s3" {
       encrypt = true
-      bucket = "${var.tf_state_save_bucket}"
-      dynamodb_table = "${var.tf_state_lock_db}"
+      bucket = "ghost-tf-state-bucket78277"
+      dynamodb_table = "tf-state-lock-db"
       key = "path/path/terraform.tfstate"
-      region = "${var.primary_region}"
+      region = "eu-central-1"
   }
 }
 
